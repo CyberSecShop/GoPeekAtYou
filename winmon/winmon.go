c106fb5ca2b8ab038db15efd7c02d937231c7d8e
FarrOut <4e4f9ce1@opayq.com>
2023-06-23 14:01:47 +0200

    opening event handler

--- winmon/winmon.go
+++ winmon/winmon.go
@@ -1,6 +1,7 @@
 package winmon
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	log "github.com/sirupsen/logrus"
@@ -25,9 +26,18 @@ func Start(name string) (int, error) {
 	handle, err := Open(name)
 	if err != nil {
 		log.Error("Monitoring start failed! Because " + err.Error())
+		return -2, err
 	} else {
 		handleStr := fmt.Sprint(uintptr(handle))
 		log.Info("Monitoring started. Event log handle: " + handleStr)
+
+		json, fetchErr := FetchInfo(handle)
+		if fetchErr != nil {
+			log.Error("Fetching info failed! Because " + fetchErr.Error())
+			return -3, fetchErr
+		} else {
+			log.Debug("Info retrieved:\n%s", json) //TODO testing!
+		}
 	}
 
 	return 0, nil // All good, return error code 0
@@ -60,4 +70,27 @@ func Open(name string) (windows.Handle, error) {
 
 	log.Debug("Opened Event Log handler for " + name)
 	return handle.Handle, nil // All good, return error code 0
+}
+
+func FetchInfo(handle windows.Handle) ([]byte, error) {
+	log.Debug("Fetching info for Event Log handler " + fmt.Sprint(uintptr(handle)))
+
+	// TODO testing
+	raw := map[string]interface{}{
+		"intValue":    1234,
+		"boolValue":   true,
+		"stringValue": "hello!",
+		"objectValue": map[string]interface{}{
+			"arrayValue": []int{1, 2, 3, 4},
+		},
+	}
+
+	json, err := json.Marshal(raw)
+	if err != nil {
+		log.Warn("could not marshal json: %s\n", err)
+		return nil, err
+	}
+
+	log.Debug("Done fetching info for Event Log handler " + fmt.Sprint(uintptr(handle)))
+	return json, nil // All good, return error code 0
 }
